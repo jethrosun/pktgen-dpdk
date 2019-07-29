5bfdd0c42e04dc1db903d733c6571dc9931ab4ad
jethros <jethro.sun7@gmail.com>
2019-07-29 13:54:20 -0400

    simple

--- prun.sh
+++ prun.sh
@@ -24,5 +24,9 @@
 #sudo -E app/x86_64-native-linuxapp-gcc/pktgen -l 0-4 -n 3 -- -P -T -m "[1:3].0, [2:4].1"  -s 0:large-traces/top500-trace2.pcap # wrong MTU
 
 # Working large trace (first part)
+# 382 M
+#sudo -E app/x86_64-native-linuxapp-gcc/pktgen -l 0-4 -n 3 -- -P -T -m "[1:3].0, [2:4].1"  -s 0:large-traces/huge-trace.pcap
+# 131 M
 sudo -E app/x86_64-native-linuxapp-gcc/pktgen -l 0-4 -n 3 -- -P -T -m "[1:3].0, [2:4].1"  -s 0:large-traces/part1-huge-trace.pcap
+# 42 M
 #sudo -E app/x86_64-native-linuxapp-gcc/pktgen -l 0-4 -n 3 -- -P -T -m "[1:3].0, [2:4].1"  -s 0:large-traces/fiftyk.pcap
